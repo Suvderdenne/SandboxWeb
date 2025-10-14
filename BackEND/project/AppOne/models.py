fc0e4e3684343e708b2f3e517aeaf943806299dd
khuslenne27 <150325978+khuslenne27@users.noreply.github.com>
2025-10-14 15:21:11 +0800

    user model done

---
+++ BackEND/project/AppOne/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
